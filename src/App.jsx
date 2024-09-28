a7e7aa4f2c3d7b1161fb03bd63b6e0b72af52072
DanThePanMan <danielchenschw@gmail.com>
2024-09-28 18:31:58 -0400

    Create Template for NewTask

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import CalendarSection from "./components/CalendarSection";
+import NewTask from "./components/NewTask";
 import Sidebar from "./components/Sidebar";
 import "@fontsource/inter";
 
 function App() {
     return (
-        <div className="flex w-[100vw] h-[100vh] flex-row">
-            <Sidebar />
-            <CalendarSection />
-        </div>
+        <>
+            <div className="flex w-[100vw] h-[100vh] flex-row">
+                <Sidebar />
+                <CalendarSection />
+            </div>
+            <NewTask />
+        </>
     );
 }
 
---
+++ src/components/NewTask.jsx
@@ -0,0 +1,5 @@
+export default function NewTask() {
+    return (
+        <button className="w-16 h-16 rounded-full absolute top-[80vh] left-16 bg-main"></button>
+    );
+}
